2eeaadc23a5da98a873dc97b3266f39e92a96eae
DavidFdez02 <DavidFB02@outlook.com>
2017-05-17 14:50:59 +0200

    Fin

---
+++ app/src/main/java/davidfdez/appp2ifinal/activity/AdminSQLiteOpenHelper.java
@@ -0,0 +1,27 @@
+package davidfdez.appp2ifinal.activity;
+
+/**
+ * Created by David on 12/05/2017.
+ */
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+
+public class AdminSQLiteOpenHelper  extends SQLiteOpenHelper {
+    public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL("create table User (id text Primary Key, password text,language text)");
+       // db.execSQL("create table Mesure (idMesure text Primary Key, FOREIGN KEY(idUser) REFERENCES User(id), CO2messure INTEGER, Luminosite INTEGER, latitude Double, longitude double, Temperature INTEGER ");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
---
+++ app/src/main/java/davidfdez/appp2ifinal/activity/SignIn.java
@@ -0,0 +1,102 @@
+package davidfdez.appp2ifinal.activity;
+
+import android.content.ContentValues;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+import android.content.Intent;
+
+import davidfdez.appp2ifinal.R;
+
+public class SignIn extends AppCompatActivity {
+    private EditText et1,et2;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_signin);
+
+        et1=(EditText)findViewById(R.id.etUser);
+        et2=(EditText)findViewById(R.id.etPass);
+    }
+
+    public void signIn(View v) {  //hacer que no se repita comprobando si ya existe
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
+                "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String cod = et1.getText().toString();
+        String descri = et2.getText().toString();
+        ContentValues registro = new ContentValues();
+        registro.put("id", cod);
+        registro.put("password", descri);
+        registro.put("language", "eng");
+        if(!checkIfUserExists(cod)) {
+            bd.insert("User", null, registro);
+            bd.close();
+            et1.setText("");
+            et2.setText("");
+            Toast.makeText(this, "User Signed In",
+                    Toast.LENGTH_SHORT).show();
+        }
+        else{
+            Toast.makeText(this, "User already exists",
+                    Toast.LENGTH_SHORT).show();
+
+        }
+    }
+    public boolean checkIfUserExists(String User) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
+                "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        boolean exists = false;
+        Cursor fila = bd.rawQuery(
+                "select id from User where id = '" + User + "'", null);
+        if (fila.moveToFirst()) {
+            if(fila.getString(0).equals(User) ){
+                exists =  true;
+            }
+        } else {
+            exists = false;
+        }
+        return exists;
+    }
+
+    public void logIn(View v) {
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
+                "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        String user = et1.getText().toString();
+        String pass = et2.getText().toString();
+        Cursor fila = bd.rawQuery(
+                "select id,password from User where id = '" + user + "'", null);
+        if (fila.moveToFirst()) {
+            if (fila.getString(0).equals(user) && fila.getString(1).equals(pass)) {
+                Toast.makeText(this, "Password matchs the user",
+                        Toast.LENGTH_SHORT).show();
+                Intent i=new Intent(this,MainActivity.class);
+                i.putExtra("user", et1.getText().toString());
+                startActivity(i);
+
+            } else
+                Toast.makeText(this, "Error",
+                        Toast.LENGTH_SHORT).show();
+        }
+        else
+            Toast.makeText(this, "Error",
+                    Toast.LENGTH_SHORT).show();
+
+        bd.close();
+    }
+
+    public void reset(View v){
+        AdminSQLiteOpenHelper admin = new AdminSQLiteOpenHelper(this,
+                "administracion", null, 1);
+        SQLiteDatabase bd = admin.getWritableDatabase();
+        bd.execSQL("delete from User" );
+        bd.close();
+
+    }
+}
